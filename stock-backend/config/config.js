06c746cf7734e7d7bdb43296e20a58a9617eb0c1
gabaeliz <elizabethg.eg12@gmail.com>
2023-08-28 10:38:03 -0600

    Conexion a base de datos

--- stock-backend/config/config.js
+++ stock-backend/config/config.js
@@ -1,5 +1,15 @@
-require("dotenv").config();
-
-module.exports = {
-    PORT: process.env.PORT
-}
+require("dotenv").config(); // Módulo que carga variables de entorno desde un archivo .env a process.env
+//Sustituir valores de host, user, name, password con los process.env una vez que tengamos el archivo .env con los valores a sustituir
+module.exports = 
+{
+  PORT: process.env.PORT || 5000,
+  DB: {
+    PORT: process.env.DB_PORT || 5432,
+    HOST: process.env.DB_HOST || '127.0.0.1',
+    USER: process.env.DB_USER,
+    NAME: process.env.DB_NAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    DIALECT: "postgres",
+    ENV: "development",
+    seederStorageTableName: "sequelize_data"
+  }};
--- stock-backend/index.js
+++ stock-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const server = express();
 const { PORT } = require("./config/config.js");
+const { db } = require("./config/database.js");
 
-server.listen(PORT, () =>{
-    console.log(`The server is listening on ${PORT}`);
+db.authenticate().then(() => {
+    console.log("Database conneted");
+    server.listen(PORT, () => {
+        console.log(`The server is listening on ${PORT}`);
+    });
 });
